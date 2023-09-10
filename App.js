f5f4f665939e60a38afa9821055c6c1ca70594c8
Nham <nguyenlamhai89@gmail.com>
2023-09-10 11:58:31 +0700

    App Updated

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Image,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  console.log("App is Running");
+  const handlePress = () => console.log("Pressed");
+  const feedbackButton = () => Alert.alert("Fuck you");
+
   return (
-    <View style={styles.container}>
-      <Text>Hallo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello React Native - A really really long text. Now I wanna make this
+        even longer and see what happens!
+      </Text>
+      <Image source={require("./assets/favicon.png")} />
+      <Button title="Press me" onPress={feedbackButton} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "orange",
   },
 });
